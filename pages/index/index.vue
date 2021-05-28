964ccd059ef4f4440592cbd2b1cd0673ad0ebfe3
JianK <JianK6661@163.com>
2021-05-28 14:08:56 +0800

    created

---
+++ pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="content">
+		<button type="default" @click="open">open</button>
+		<sofar-picker :visable.sync="pickerVisable" :defaultDate="defaultDate" @confirm="confirm"></sofar-picker>
+	</view>
+</template>
+
+<script>
+	import sofarPicker from '@/components/sofar-picker/sofar-picker.vue'
+	export default {
+		components: {
+			sofarPicker
+		},
+		data() {
+			return {
+				pickerVisable: false,
+				defaultDate: ['2020.1', '至今'],
+				dateValue: []
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			open() {
+				this.pickerVisable = true
+			},
+			confirm(date) {
+				this.dateValue = date
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
