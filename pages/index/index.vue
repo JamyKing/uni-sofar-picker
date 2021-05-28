c58effa4c8c515c4fab52ddbe04b926c59906bb6
JianK <JianK6661@163.com>
2021-05-28 14:32:12 +0800

    update

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,10 @@
 <template>
 	<view class="content">
-		<button type="default" @click="open">open</button>
+		<view class="header">
+			<view class="date-item">日期范围：{{dateValue[0]}} - {{dateValue[1]}}</view>
+			<view class="date-item">参数值(Array)：{{dateValue}}</view>
+		</view>
+		<button class="date-btn" type="default" @click="openPicker">打开</button>
 		<sofar-picker :visable.sync="pickerVisable" :defaultDate="defaultDate" @confirm="confirm"></sofar-picker>
 	</view>
 </template>
@@ -18,11 +22,8 @@
 				dateValue: []
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-			open() {
+			openPicker() {
 				this.pickerVisable = true
 			},
 			confirm(date) {
@@ -33,5 +34,18 @@
 </script>
 
 <style>
-	
+	.header {
+		width: 100%;
+		padding: 60rpx 30rpx;
+		display: flex;
+		flex-direction: column;
+	}
+	.date-item {
+		margin-bottom: 30rpx;
+	}
+	.date-btn {
+		width: 500rpx;
+		height: 80rpx;
+		line-height: 80rpx;
+	}
 </style>
